c9a27cfc1a894a72ea934ea3ba3116b33053ee5f
Andrea-Baggio <andreabaggio800@gmail.com>
2022-11-24 16:09:12 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <header-page-vue></header-page-vue>
+    <main-page-vue></main-page-vue>
+    <footer-page-vue></footer-page-vue>
+  </div>
+</template>
+
+<script>
+import HeaderPageVue from './components/HeaderPage.vue';
+import MainPageVue from './components/MainPage.vue';
+import FooterPageVue from './components/FooterPage.vue';
+
+export default {
+  components: {
+    HeaderPageVue,
+    MainPageVue,
+    FooterPageVue,
+  },
+};
+</script>
+
+<style>
+
+</style>
